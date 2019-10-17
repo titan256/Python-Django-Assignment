69e5911f4bc5ce688dd151b4c3fd6214baac951f
Eseza Muwanga Najjombwe <43315519+titan256@users.noreply.github.com>
2019-10-17 14:36:22 +0300

    Adding Test Cases

--- Test/two/payments/momo/tests.py
+++ Test/two/payments/momo/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import MomoRequest
+from .mtn import MtnMomo
 
 # Create your tests here.
+class MtnMoMoTestCase(TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_get_transaction_status(self):
+
+		status = MtnMomo.collectionTransactionStatus('7858409e-fd71-48b9-a1a5-ca22eb9c6bfd')
+		assert isinstance(status, dict)
+		assert "amount" in status.keys()
+		assert "currency" in status.keys()
+
+	def test_request_to_pay(self):
+
+		ref = MtnMomo.collectionRequestToPay("256772123456", "600", "123456789", "dd","dd","EUR")
+		assert isinstance(ref, dict)
+		assert "transaction_ref" in ref.keys()
